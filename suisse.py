7b4b0c65de7f6febd573f3a37dfea30151ae39ea
gayicon <dvmaslovv@gmail.com>
2025-01-17 20:20:16 +0100

    Update suisse.py

--- suisse.py
+++ suisse.py
@@ -16,12 +16,24 @@ countries_data = {
 fig = px.choropleth(locations=countries_data['Country'], 
                     color=countries_data['War with Switzerland'], 
                     locationmode='country names', 
-                    title='Countries that Have Been at War with Switzerland',
-                    hover_name=countries_data['Country'])  # Add country names on hover
+                    title='Countries that Have Been at War with Switzerland')
 
-# Add country names as labels
-fig.update_geos(showcoastlines=True, coastlinecolor="Black", projection_type="mercator")
-fig.update_traces(text=countries_data['Country'], textposition="top center", marker=dict(line=dict(width=0)))
+# Add annotations (country names) to the map
+annotations = []
+for country in countries_data['Country']:
+    annotations.append(
+        dict(
+            x=fig.data[0].locationlat[countries_data['Country'].index(country)],  # Get the latitude of the country
+            y=fig.data[0].locationlon[countries_data['Country'].index(country)],  # Get the longitude of the country
+            text=country,  # Country name
+            showarrow=False,  # Do not show an arrow
+            font=dict(size=10, color='black'),  # Font size and color
+            align='center'
+        )
+    )
+
+# Add the annotations to the map
+fig.update_layout(annotations=annotations)
 
 # Streamlit app
 st.title('Interactive Map of Countries at War with Switzerland')
