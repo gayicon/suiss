a91ca05365945f26e1497787f6f01292abb7afdf
gayicon <dvmaslovv@gmail.com>
2025-01-17 20:20:57 +0100

    Update suisse.py

--- suisse.py
+++ suisse.py
@@ -1,32 +1,38 @@
 import streamlit as st
 import plotly.express as px
 
-# Expanded list of countries that have been at war with Switzerland
+# List of countries at war with Switzerland
 countries_at_war_with_switzerland = [
     "France", "Austria", "Italy", "Germany", "Spain", "Netherlands", "Sweden"
 ]
 
-# Create a dictionary with country names and war status
-countries_data = {
-    'Country': countries_at_war_with_switzerland,
-    'War with Switzerland': ['Yes'] * len(countries_at_war_with_switzerland)
+# Manually defined latitude and longitude for countries
+coordinates = {
+    "France": (46.603354, 1.888334),
+    "Austria": (47.516231, 13.550072),
+    "Italy": (41.87194, 12.56738),
+    "Germany": (51.165691, 10.451526),
+    "Spain": (40.463667, -3.74922),
+    "Netherlands": (52.3676, 4.9041),
+    "Sweden": (60.128161, 18.643501),
 }
 
 # Create the map using Plotly Express
-fig = px.choropleth(locations=countries_data['Country'], 
-                    color=countries_data['War with Switzerland'], 
+fig = px.choropleth(locations=countries_at_war_with_switzerland, 
+                    color=['Yes'] * len(countries_at_war_with_switzerland), 
                     locationmode='country names', 
                     title='Countries that Have Been at War with Switzerland')
 
 # Add annotations (country names) to the map
 annotations = []
-for country in countries_data['Country']:
+for country in countries_at_war_with_switzerland:
+    lat, lon = coordinates[country]  # Get the latitude and longitude of the country
     annotations.append(
         dict(
-            x=fig.data[0].locationlat[countries_data['Country'].index(country)],  # Get the latitude of the country
-            y=fig.data[0].locationlon[countries_data['Country'].index(country)],  # Get the longitude of the country
+            x=lon,
+            y=lat,
             text=country,  # Country name
-            showarrow=False,  # Do not show an arrow
+            showarrow=False,  # No arrow
             font=dict(size=10, color='black'),  # Font size and color
             align='center'
         )
